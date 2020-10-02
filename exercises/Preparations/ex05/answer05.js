b20b66098574e72ee3a63e0bbe4792c86072b970
Anders Bjurman <arbjurman@gmail.com>
2020-10-02 16:22:12 -0700

    add ex02 & ex05

---
+++ exercises/Preparations/ex05/answer05.js
@@ -0,0 +1,17 @@
+'The constructor for the prototype method substring, is String.'
+    String.prototype.substring()
+    String.prototype.substring
+
+'The constructor for the static method create, is Object.'
+    Object.create()
+    Object.create
+
+'The constructor for the static method fromCharCode, is String.'
+    String.fromCharCode()
+    String.fromCharCode
+
+'The constructor for the prototype method slice, is String and Array'
+    String.prototype.slice()
+    String.prototype.slice  
+    Array.prototype.slice()
+    Array.prototype.slice
