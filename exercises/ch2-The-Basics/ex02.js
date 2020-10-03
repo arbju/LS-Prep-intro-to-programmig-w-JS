1b5c670c8c72eb0bbc736cb7cbbc8f4f0199a142
Anders Bjurman <arbjurman@gmail.com>
2020-10-02 20:10:31 -0700

    exercisis ch2 started

---
+++ exercises/ch2-The-Basics/ex02.js
@@ -0,0 +1,14 @@
+let num = 4936
+let ones = num % 10
+console.log(ones)
+
+num = (num - ones) / 10
+let tens = num % 10
+console.log(tens)
+
+num = (num - tens) / 10
+let hundreds = num % 10
+console.log(hundreds)
+
+let thousands = (num - hundreds) / 10
+console.log(thousands)
